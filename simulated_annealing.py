3c3304e0dabc5ef9e677b47fefaaeafab69ff6a5
Andrew Gazelka <andrew.gazelka@gmail.com>
2019-03-12 11:59:37 -0500

    use tkinter UI

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,14 +1,12 @@
 import math
 import random
-from tkinter import Tk
-
-import matplotlib.pyplot as plt
+from tkinter import Tk, Canvas, Button
 
 point_num = 20
-max_x = 100
-max_y = 100
+max_x = 300
+max_y = 300
 
-points = [(random.random()*max_x, random.random()*max_y) for i in range(point_num)]
+points = [(random.random() * max_x, random.random() * max_y) for i in range(point_num)]
 
 
 # window = Tk()
@@ -27,30 +25,54 @@ def point_distance2(a, b):
     return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
 
 
-temp = 1
-temp_threshold = 0.1
-current = -4_000
+temp = 1_000_000
+temp_multiplier = .98
+current = -1E10
+
+root = Tk()
+root.title("Title")
+root.geometry("100x100")
+
+c = Canvas(root, width=max_x, height=max_y)
+c.pack()
+# c.pack()
+
+count = 0
 
 
-while temp > temp_threshold:
-    idx = range(len(points))
-    i1, i2 = random.sample(idx, 2)
-    points_copied = points.copy()
-    points_copied[i1], points_copied[i2] = points_copied[i2], points_copied[i1]
-    result = -distance2(points_copied)
-    exponent = result - current
-    value = math.e ** (exponent / temp)
-    if value > random.random():
-        current = result
-        points = points_copied
-    temp -= temp_threshold
+def on_click():
+    global temp
+    global points
+    global current
+    if temp > temp_multiplier:
+        c.delete("all")
+        idx = range(len(points))
+        i1, i2 = random.sample(idx, 2)
+        points_copied = points.copy()
+        points_copied[i1], points_copied[i2] = points_copied[i2], points_copied[i1]
+        result = -distance2(points_copied)
+        exponent = result - current
+
+        probability_accept = 1.0 if exponent > 0 else math.e ** (exponent / temp)
+
+        if probability_accept > random.random():
+            print("accepted", probability_accept)
+            current = result
+            points = points_copied
+        else:
+            print("denied:", probability_accept)
+            print("old: ", current, "new: ", result)
+
+        print("temp:",temp)
+
+        for i in range(len(points) - 1):
+            c.create_line(3*points[i], 3*points[i + 1])
+        c.create_line(3*points[-1], 3*points[0])
+        root.update()
+        temp *= temp_multiplier
 
 
-x = list(map(lambda x: x[0], points))
-y = list(map(lambda x: x[1], points))
+button = Button(root, text="Im Button", command=on_click)
+button.pack()
 
-plt.scatter(x, y)
-plt.plot(x, y)
-plt.grid(True)
-
-plt.show()
+root.mainloop()
